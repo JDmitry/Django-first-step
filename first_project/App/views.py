d4d494412f75347f204e19307d29a61e9fa894be
Dmitry <jdmitrys@yandex.ru>
2022-11-17 22:06:13 +0400

    Add more pathes

--- first_project/App/views.py
+++ first_project/App/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
   
 def index(request):
-    return HttpResponse("My App")
+    return HttpResponse("<h1>Index</h1>")
+
+def about(request):
+    return HttpResponse("<h1>About</h1>")
+
+def contact(reqest):
+    return HttpResponse("<h1>Contact</h1>")
