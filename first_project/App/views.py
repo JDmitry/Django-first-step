fedb9ddd177f2157f8ab8d7172d6facdb0ea7cb9
Dmitry <jdmitrys@yandex.ru>
2022-12-02 14:03:14 +0400

    function in teplate

--- first_project/App/views.py
+++ first_project/App/views.py
@@ -1,13 +1,4 @@
 from django.shortcuts import render
 
-class User:
-    def __init__(self, name):
-        self.name = name
-
 def index(request):
-    user = User("Sam")
-    data = {"user": user}
-    return render(request, "index.html", context=data)
-
-
-
+    return render(request, "index.html", context={"logos": "Logos"})
