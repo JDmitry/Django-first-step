edc92a2c0c7eddb6bf2c15498dbf292db91f95d6
Dmitry <jdmitrys@yandex.ru>
2022-12-06 19:11:11 +0400

    Tamplate from urls.py

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.urls import path
 from App import views
+from django.views.generic import TemplateView
   
 urlpatterns = [
     path("", views.index),
+    path("about/", TemplateView.as_view(template_name="about.html", extra_context={"header": "Goida"})),
+    path("contact/", TemplateView.as_view(template_name="contact.html"))
 ]
