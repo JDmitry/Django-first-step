e4505dd68b74fe701344d68c4abfee1568f73315
Dmitry <jdmitrys@yandex.ru>
2022-12-07 19:24:20 +0400

    Change template directory

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -19,6 +19,4 @@ from django.views.generic import TemplateView
   
 urlpatterns = [
     path("", views.index),
-    path("about/", TemplateView.as_view(template_name="about.html", extra_context={"header": "Goida"})),
-    path("contact/", TemplateView.as_view(template_name="contact.html"))
 ]
