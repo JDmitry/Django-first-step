3c74e8bfe3b6a39a99c0a8db210aff5fe1c1fa9d
Dmitry <jdmitrys@yandex.ru>
2022-12-12 21:26:23 +0400

    Extented taplate

--- first_project/App/views.py
+++ first_project/App/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
   
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", context={"name": "Earth"})
  
 def contact(request):
     return render(request, "contact.html")
