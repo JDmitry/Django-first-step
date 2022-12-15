12babc38c62d6a5ab90b3fa934777e610bb1d324
Dmitry <jdmitrys@yandex.ru>
2022-12-15 20:11:23 +0400

    Work with forms

--- first_project/App/forms.py
+++ first_project/App/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
 class UserForm(forms.Form):
-    name = forms.CharField()
-    age = forms.IntegerField()
+    name = forms.CharField(label="Name", help_text="Enter the name")
+    num = forms.CharField(label="Num", help_text="Enter the num", initial=0)
+    comment = forms.CharField(label="Comments", widget=forms.Textarea)
+    field_order = ["name", "num", "comment"]
--- first_project/App/views.py
+++ first_project/App/views.py
@@ -5,8 +5,9 @@ from .forms import UserForm
 def index(request):
     if request.method == "POST":
         name = request.POST.get("name")
-        age = request.POST.get("age")
-        return HttpResponse(f"<h1>Name: {name}, age: {age}</h1>")
+        comment = request.POST.get("comment")
+        num = request.POST.get("num")
+        return HttpResponse(f"<h1>Name: {name}    -   Num:  {num}  - Comment: {comment}</h1>")
     else:
-        userform = UserForm()
+        userform = UserForm(field_order = ["name", "comment", "num"])
         return render(request, "index.html", {"form": userform})
