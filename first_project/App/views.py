c069c537778b0c832d8cf85eb38f371416d25569
Dmitry <jdmitrys@yandex.ru>
2022-12-26 20:30:48 +0400

    Use QuerySet API

--- first_project/App/views.py
+++ first_project/App/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import UserForm
+from .models import Person
  
+# получаем все объекты
+people = Person.objects.all()
+print(people.query)
+ 
+# получаем объекты с именем Tom
+people = people.filter(name = "Tom")
+print(people.query)
+ 
+# получаем объекты с возрастом, равным 31
+people = people.filter(age = 31)
+print(people.query)
+ 
+# здесь происходит выполнения запроса в БД
+for person in people:
+    print(f"{person.id}.{person.name} - {person.age}")
+
 def index(request):
-    userform = UserForm()
-    if request.method == "POST":
-        userform = UserForm(request.POST)
-        if userform.is_valid():
-            name = userform.cleaned_data["name"]
-            return HttpResponse(f"<h2>Hello, {name}</h2>")
     return render(request, "index.html", {"form": userform})
