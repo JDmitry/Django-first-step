11879fe9216d9d5b06be803c4974e44b31bd6d44
Dmitry <jdmitrys@yandex.ru>
2022-12-27 19:28:23 +0400

    Add data into the database

--- first_project/App/views.py
+++ first_project/App/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Person
- 
-# получаем все объекты
-people = Person.objects.all()
-print(people.query)
- 
-# получаем объекты с именем Tom
-people = people.filter(name = "Tom")
-print(people.query)
- 
-# получаем объекты с возрастом, равным 31
-people = people.filter(age = 31)
-print(people.query)
- 
-# здесь происходит выполнения запроса в БД
-for person in people:
-    print(f"{person.id}.{person.name} - {person.age}")
 
-def index(request):
-    return render(request, "index.html", {"form": userform})
+rick = Person(name="Rick", age=45)
+rick.save()
